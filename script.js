a94031b2ebcfdaae3fb81e5996368ce667f4e496
harsh makvana <harsh.makvana.ciyaza@gmail.com>
2023-08-16 09:30:01 +0530

    CiyazaWebsite

---
+++ script.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+
+hamburger.addEventListener("click", ()=>{
+    menu.classList.toggle("hidden")
+    hamburger.classList.toggle("bg-white")
+})
+       
+var toTopButton = document.getElementById("to-top-button");
+
+        // When the user scrolls down 200px from the top of the document, show the button
+        window.onscroll = function () {
+            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+                toTopButton.classList.remove("hidden");
+            } else {
+                toTopButton.classList.add("hidden");
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function goToTop() {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      spacing:{
+        "big":"48rem"
+      },
+      screens:{
+        sm:"480px",
+        md:"768px",
+        lg:"1024px",
+        xl:"1280px"
+      }
+     
+    },
+  },
+  plugins: [],
+}
+
+function newFunction() {
+  return "48rem";
+}
+
